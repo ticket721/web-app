b181b286e42c01dd4c3b9d499d387535707d54a3
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2019-02-18 18:38:02 -0500

    [+~] Setup ReactTS and NextTS
    WEB #1

---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+
+export default class t721Document extends Document {
+
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return { ...initialProps }
+    }
+
+    render() {
+        return (
+            <html>
+            <Head>
+                <style>{`body { margin: 0 } /* custom! */`}</style>
+            </Head>
+            <body className="custom_class">
+            <Main />
+            <NextScript />
+            </body>
+            </html>
+        )
+    }
+}
