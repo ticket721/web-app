28175b07918c7df64c1bb33f5b9fb4aa81a7753f
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2019-02-18 18:54:47 -0500

    [~+] TSLint and link-module-alias setup
      - TSLint for the tsx files
      - link-module-alias to create the @components link
    
    WEB #1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Test } from '@components/Test';
 
 export default class extends React.Component {
-    render() {
+    render(): React.ReactNode {
         return (
             <div>
                 Hello Next.js
+                <Test/>
             </div>
-        )
+        );
     }
 }
