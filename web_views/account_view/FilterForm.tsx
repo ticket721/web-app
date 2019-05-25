39c3e59bd614633f662d836687bd6d6f1b70b0cb
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2019-05-25 12:58:28 -0400

    feat: fuzzy search in event list in account

--- web_views/account_view/FilterForm.tsx
+++ web_views/account_view/FilterForm.tsx
@@ -2,6 +2,7 @@ import { StrapiEvent }        from '@utils/strapi/event';
 import * as React             from 'react';
 import { Select, Typography } from 'antd';
 import { I18N, I18NProps }    from '../../utils/misc/i18n';
+import FuzzySearch from 'fuzzy-search';
 
 const Option = Select.Option;
 
@@ -17,9 +18,17 @@ export interface FilterFormProps {
     set_event: (events: number[]) => void;
 }
 
+interface FilterFormState {
+    search: string;
+}
+
 type MergedFilterFormProps = FilterFormProps & I18NProps;
 
-class FilterForm extends React.Component<MergedFilterFormProps> {
+class FilterForm extends React.Component<MergedFilterFormProps, FilterFormState> {
+
+    state: FilterFormState = {
+        search: null
+    };
 
     on_event_change = (value: number[]): void => {
         if (value.length === 0) {
@@ -28,18 +37,47 @@ class FilterForm extends React.Component<MergedFilterFormProps> {
         this.props.set_event(value);
     }
 
-    render(): React.ReactNode {
+    on_search = (search: string): void => {
+        if (!search || search.length === 0) {
+            this.setState({
+                search: null
+            });
+        } else {
+            this.setState({
+                search
+            });
+        }
 
+    }
+
+    render(): React.ReactNode {
         const event_options = this.props.events
 
-            ?
-            this.props.events.map((event: StrapiEvent, idx: number): React.ReactNode =>
-                <Option key={idx} value={event.id}>
-                    <Typography.Text>{event.name}</Typography.Text>
-                    <Typography.Text style={{marginLeft: 12, color: '#aaaaaa'}}>#{event.id}</Typography.Text>
+            ? (
+                this.state.search !== null
+                    ?
 
-                </Option>)
+                    (new FuzzySearch(this.props.events, ['name'], {
+                        sort: true
+                    }))
+                        .search(this.state.search)
+                        .map((event: StrapiEvent, idx: number): React.ReactNode =>
+                            <Option key={idx} value={event.id}>
+                                <Typography.Text>{event.name}</Typography.Text>
+                                <Typography.Text style={{marginLeft: 12, color: '#aaaaaa'}}>#{event.id}</Typography.Text>
 
+                            </Option>)
+
+                    :
+                    this.props.events
+                        .map((event: StrapiEvent, idx: number): React.ReactNode =>
+                            <Option key={idx} value={event.id}>
+                                <Typography.Text>{event.name}</Typography.Text>
+                                <Typography.Text style={{marginLeft: 12, color: '#aaaaaa'}}>#{event.id}</Typography.Text>
+
+                            </Option>)
+
+            )
             :
             [];
 
@@ -50,7 +88,7 @@ class FilterForm extends React.Component<MergedFilterFormProps> {
                 </Typography.Text>
                 <br/>
                 <br/>
-                <Select style={{width: '90%', marginLeft: '5%', float: 'left'}} mode='multiple' onChange={this.on_event_change} value={this.props.options.event ? this.props.options.event : []}>
+                <Select notFoundContent={null} showSearch={true} filterOption={false} onSearch={this.on_search} style={{width: '90%', marginLeft: '5%', float: 'left'}} mode='multiple' onChange={this.on_event_change} value={this.props.options.event ? this.props.options.event : []}>
                     {event_options}
                 </Select>
             </div>
