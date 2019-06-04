645beec90d24b1c9ada8a8c3cee9e3a8f8ab87ac
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2019-06-03 22:16:49 -0400

    fix: change config dir name

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ npm install -g portalize
 
 curl -u "$NEXUS_USERNAME:$NEXUS_PASSWORD" "$NEXUS_ENDPOINT/repository/$NEXUS_REPOSITORY/artifacts/latest.tar.gz" -o portal.tar.gz
 
-env PORTALIZE_ARCHIVE_NAME=portal.tar.gz portalize restore ./build/portalize.config.json
+env PORTALIZE_ARCHIVE_NAME=portal.tar.gz portalize restore ./build_config/portalize.config.json
 
 rm portal.tar.gz
 
-docker build -f ./build/Dockerfile -t webapp .
+docker build -f ./build_config/Dockerfile -t webapp .
 
 rm -rf portal
 
